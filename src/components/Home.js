39b8ab59794c02754ca162e1eadbc9de72711666
Nick Huemmer <njhuemmer@gmail.com>
2021-10-14 10:51:03 -0400

    commiting changes to home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,9 +12,6 @@ class Home extends Component {
         <h3>This is a fun tool for looking up pet pictures on the photo-sharing site,</h3>
         <img src={flickrLogo} className ="flickr-logo" alt="Flickr logo"/>
         <p>It uses the Flickr API to find photos of pets or whatever you put in the search box above.</p>
-        
-        
-        
         <Photo title="🐟🐢🐾Pets 💞🐈‍🐕 " pics={this.props.pics} />
 
       </div>
