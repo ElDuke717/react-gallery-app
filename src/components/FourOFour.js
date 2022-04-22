d415159d697e88c70ba2ad8fc11ba6fc5c2b4c98
Nick <njhuemmer@gmail.com>
2022-04-22 15:32:10 -0400

    testing 404 message paw color change to HSL hsl(200, 98%, 37%) to see if changes are pushed to github pages, reverting home directory

--- src/components/FourOFour.js
+++ src/components/FourOFour.js
@@ -2,20 +2,20 @@ import React from 'react';
 import DogPaw from './DogPaw';
 
 const FourOFour = () => {
-    const pawColor =   '#BD0202';
-    const pawHeight = 100;
-    const pawWidth = 100; 
-    return (
-   
-    <div className="fourofour">
-    
-    <DogPaw width={pawWidth} height={pawHeight} fill={pawColor} />
-    <h1><strong>404</strong></h1>
-    <h2>Page Not Found</h2>
+  const pawColor = 'hsl(200, 98%, 37%)';
+  const pawHeight = 100;
+  const pawWidth = 100;
+  return (
 
-    <p>This page could not be found.</p>
-  </div>
-);
+    <div className="fourofour">
+
+      <DogPaw width={pawWidth} height={pawHeight} fill={pawColor} />
+      <h1><strong>404</strong></h1>
+      <h2>Page Not Found</h2>
+
+      <p>This page could not be found.</p>
+    </div>
+  );
 }
 
 export default FourOFour
