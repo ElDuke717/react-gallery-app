db18cb36d3f2927d14f72f886b9aa7c7c6506285
Nick <njhuemmer@gmail.com>
2022-04-22 14:40:34 -0400

    trying exact path for react-gallery-app route

--- src/App.js
+++ src/App.js
@@ -160,8 +160,8 @@ export default class App extends Component {
 
               <Switch>
                 {/* The home route "/" renders the home view and dynamically updates the title based on the search term */}
-                <Route exact path="/" render={() => <Home pics={this.state.pics} title={this.state.defaultTerm} petPics={this.state.petsLoading} />} />
                 <Route exact path="/react-gallery-app" render={() => <Home pics={this.state.pics} title={this.state.defaultTerm} petPics={this.state.petsLoading} />} />
+                <Route path="/" render={() => <Home pics={this.state.pics} title={this.state.defaultTerm} petPics={this.state.petsLoading} />} />
                 <Route path="/puppies" render={() => <Puppies pics={this.state.puppyPics} />} />
                 <Route path="/dogs" render={() => <Dogs pics={this.state.dogPics} />} />
                 <Route path="/cats" render={() => <Cats pics={this.state.catPics} />} />
